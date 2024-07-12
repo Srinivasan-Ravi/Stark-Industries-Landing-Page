4cdbef5d9d0db9f90e7c67fe0617d89f4b78034a
Srinivasan R <124020324+Srinivasan-Ravi@users.noreply.github.com>
2024-07-12 10:25:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('learn-more-btn').addEventListener('click', () => {
+    alert('More information will be coming soon!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Message sent!');
+});
